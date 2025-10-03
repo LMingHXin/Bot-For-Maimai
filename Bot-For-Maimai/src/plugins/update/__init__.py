9d1fc3bfd27cbb82424edc728f02f3cc19f9f772
Ming Xin <92614729+LMingHXin@users.noreply.github.com>
2025-10-03 16:18:17 +0800

    update new plugins

---
+++ Bot-For-Maimai/src/plugins/update/__init__.py
@@ -0,0 +1,36 @@
+from nonebot import get_plugin_config, on_command
+from nonebot.typing import T_State
+from nonebot.adapters.onebot.v11 import Message, Event, Bot
+from nonebot.plugin import PluginMetadata
+
+from .config import Config
+
+import git
+
+__plugin_meta__ = PluginMetadata(
+    name="update",
+    description="",
+    usage="",
+    config=Config,
+)
+
+config = get_plugin_config(Config)
+
+update = on_command("update", priority=0, block=True, aliases={"更新"})
+
+class GitRepo():
+    def __init__(self, path: str):
+        self.repo = git.Repo(path)
+
+    def pull(self):
+        origin = self.repo.remotes.main
+        origin.pull()
+
+@update.handle()
+async def _(bot: Bot, event: Event, state: T_State):
+    usrid = event.get_user_id()
+    if str(usrid) != str(config.master_qq):
+        await update.finish(Message("只有主人可以使用此命令"))
+    repo = GitRepo(path="/home/sa/Bot-For-Maimai")
+    repo.pull()
+    await update.finish("更新成功，请重启生效")
---
+++ Bot-For-Maimai/src/plugins/update/config.py
@@ -0,0 +1,5 @@
+from pydantic import BaseModel
+
+
+class Config(BaseModel):
+    master_qq: int = 2654625014  # 主人QQ
