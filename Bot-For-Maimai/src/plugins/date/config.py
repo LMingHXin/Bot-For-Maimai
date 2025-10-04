e756abbb7de26344a4d7893c0a241d75e3009d22
Ming Xin <rminghxin@163.com>
2025-10-04 21:18:01 +0800

    add new group allow use plugin date

--- Bot-For-Maimai/src/plugins/date/config.py
+++ Bot-For-Maimai/src/plugins/date/config.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel
 
 
 class Config(BaseModel):
-    date_group: list = [1060336528]  # 可以使用约会功能的群
+    date_group: list = [1060336528, 1025615469]  # 可以使用约会功能的群
     admin_group: list = [1060336528]  # 可以使用管理功能的
     admin: list = [2654625014, 1619445266]  # 管理员QQ号
