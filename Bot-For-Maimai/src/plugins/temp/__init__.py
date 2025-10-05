9377d7950ea6fc43c3f2088a5271d323c3db2895
Ming Xin <rminghxin@163.com>
2025-10-05 09:21:16 +0800

    Update __init__.py

--- Bot-For-Maimai/src/plugins/temp/__init__.py
+++ Bot-For-Maimai/src/plugins/temp/__init__.py
@@ -1,5 +1,6 @@
 from nonebot import get_plugin_config
 from nonebot.plugin import PluginMetadata
+import time, nonebot
 
 from .config import Config
 
@@ -20,7 +21,17 @@ print(bot)
 print("temp插件加载成功")
 async def temp_send():
     await bot.send_private_msg(user_id=2654625014, message="temp插件加载成功")
-print("temp插件发送消息成功")
+import asyncio
+
+t = time.gmtime()
+ts = time.strftime("%M", t)
+
+async def main():
+    if ts == "22":
+        await temp_send()
+        print("temp插件发送消息成功")
+
+asyncio.run(main())
 
 #这里是临时测试代码
   
