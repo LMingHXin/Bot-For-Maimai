f63b79b4634624387d25239d5d48e7735afcbc7b
Ming Xin <rminghxin@163.com>
2025-10-22 21:51:55 +0800

    Update config.py

--- Bot-For-Maimai/src/plugins/defend_withdraw/config.py
+++ Bot-For-Maimai/src/plugins/defend_withdraw/config.py
@@ -2,4 +2,4 @@ from pydantic import BaseModel
 
 
 class Config(BaseModel):
-    access_groups: list[int] = [106033652, 1025615469]  # 可使用该功能的群号列表
+    access_groups: list[int] = [1060336528, 1025615469]  # 可使用该功能的群号列表
