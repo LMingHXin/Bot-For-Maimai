38dc223346b65f7ecda7ed19e7f5ac7ed82ac4ae
Ming Xin <rminghxin@163.com>
2025-11-04 12:58:50 +0800

    Update __init__.py

--- Bot-For-Maimai/src/plugins/check_in/__init__.py
+++ Bot-For-Maimai/src/plugins/check_in/__init__.py
@@ -2,6 +2,7 @@ from nonebot import get_plugin_config, on_command
 from nonebot.plugin import PluginMetadata
 from nonebot.adapters.onebot.v11 import Message, Event, Bot
 from nonebot.typing import T_State
+from nonebot.params import CommandArg
 
 from .config import Config
 from .data import CheckInData, Time
@@ -24,15 +25,22 @@ check_in = on_command("check_in", aliases={"签到", "qd"})
 async def handle_check_in(bot: Bot, event: Event, state: T_State):
     group_id = int(event.group_id)  # type: ignore
     user_id = int(event.get_user_id())
+    msg_id = event.message_id # type: ignore
     ckid = CheckInData(user_id, group_id)
     point = PointsData(user_id, group_id)
     t = Time(user_id, group_id)
     if t.check_time():
         last_check_in_time = time.localtime(ckid.get_user_data())
         formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", last_check_in_time)
-        await check_in.finish(f"你今天已经签到过了哦~ 上次签到时间：{formatted_time}")
+        await bot.send_group_msg(
+            group_id=group_id,
+            message=f"[reply:id={msg_id}]你今天已经签到过了哦~ 上次签到时间：{formatted_time}"
+        )
     else:
         points_earned = random.randint(1, 10)
         point.update_points(points_earned)
         ckid.update_check_in()
-        await check_in.finish(f"签到成功！恭喜获得{points_earned}点基础积分！\n您当前总积分为：{point.get_points():.2f}点！")
+        await bot.send_group_msg(
+            group_id=group_id,
+            message=f"[reply:id={msg_id}]签到成功！恭喜获得{points_earned}点基础积分！\n您当前总积分为：{point.get_points():.2f}点！"
+        )
