8dfda948a91ff55b8dca7209250b58fbad669629
Ming Xin <rminghxin@163.com>
2025-11-04 12:53:03 +0800

    add help, update version

--- Bot-For-Maimai/src/plugins/points/__init__.py
+++ Bot-For-Maimai/src/plugins/points/__init__.py
@@ -17,3 +17,18 @@ __plugin_meta__ = PluginMetadata(
 
 config = get_plugin_config(Config)
 
+help = on_command("points_help", aliases={"积分帮助", "jf_help"})
+
+@help.handle()
+async def handle_help(bot: Bot, event: Event, state: T_State):
+    help_text = (
+        "积分系统使用说明：\n"
+        "1. 签到获取基础积分，每日可签到一次。\n"
+        "2. 积分可用于兑换面包或其他奖励。\n"
+        "3. 面包等级越高，签到时获得的积分越多。\n"
+        "4. 使用命令 'check_in' 进行签到。\n"
+        "5. 使用命令 'points' 查询当前积分。"
+        "积分结算机制：(面包等级/20 + 1) * 基础积分"
+    )
+    await help.finish(help_text)
+
